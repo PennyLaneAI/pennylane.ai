4c6152d191b9d6722cd06eb162ad9a2ab5a736e0
Josh Izaac <josh146@gmail.com>
2022-01-14 14:48:10 +0800

    Update compile.py

--- compile.py
+++ compile.py
@@ -80,6 +80,61 @@ FAQ = [
 ]
 
 
+CARNIVAL_FAQ = [
+        (
+        "When will PennyLane Quantum Carnival take place?",
+        "November 1-30, 2021. "
+    ),
+    (
+        "What is the schedule?",
+        "Check our our <a href=\"attractions.html\">attractions</a> page for more details. "
+        "Also watch out for special announcements throughout the month."
+    ),
+    (
+        "Is there a deadline for registration?",
+        "November 30th 2021 5:59pm ET"
+    ),
+    (
+        "Are there prizes?",
+        "Absolutely! The prize booth will become available during the event. "
+        "Check <a href=\"scavenger_hunt.html\">here</a> for more "
+        "details on how to win prizes."
+    ),
+    (
+        "How do I know if I have been successfully registered?",
+        "You should have seen a confirmation message on your screen after submitting "
+        "our <a href=\"register.html\">sign-up form</a>."
+    ),
+    (
+        "Do I need to register in order to participate in the Talent Show?",
+        "Yes, you will need to <a href=\"register.html\">register</a> for "
+        "the Carnival before submitting your entry to the Talent Show. "
+        "All we need is your email."
+    ),
+    (
+        "How do I get tickets in the Scavenger Hunt?",
+        "You can get tickets by attending our live events, contributing to "
+        "PennyLane, participating in the Talent Show, and if you search carefully, "
+        "you can find many other tickets hidden in secret places."
+    ),
+    (
+        "How many events are there?",
+        "Several key events are listed on the "
+        "<a href=\"attractions.html\">attractions page</a>. "
+        "Other announcements will be made on the PennyLane Twitter account, so "
+        "<a href=\"https://twitter.com/intent/user?screen_name=pennylaneai\">follow us</a> "
+        "to stay informed. "
+        "When you <a href=\"register.html\">register</a> you will also have the option "
+        "to receive email notifications with updates about the Carnival."
+    ),
+    (
+        "How many tickets are there in total?",
+        "In total, there are 100 unique points which can be won by earning "
+        "or finding tickets."
+    ),
+]
+
+
 def render_templates():
     files = {
         "index.html": {
@@ -111,15 +166,15 @@ def render_templates():
             "thumbnail": "https://pennylane.ai/img/thumbnail.png",
             "root": "."
         },
-        "faq.html": {
-            "faq_page": True,
-            "title": "Frequently Asked Questions (FAQ) &#8212; PennyLane Quantum Carnival",
-            "canonical_url": "https://pennylane.ai/carnival/faq.html",
-            "description": "Curious? Find all the details about PennyLane Quantum Carnival.",
-            "thumbnail": "https://pennylane.ai/img/thumbnail.png",
-            "root": ".",
-            "faq": FAQ,
-        },
+         "faq.html": { 
+             "faq_page": True, 
+             "title": "Frequently Asked Questions (FAQ) &#8212; PennyLane", 
+             "canonical_url": "https://pennylane.ai/faq.html", 
+             "description": "Get quick answers to your most burning questions on PennyLane and " 
+                            "quantum machine learning.", 
+             "thumbnail": "https://pennylane.ai/img/thumbnail.png", 
+             "faq": FAQ, 
+         }, 
         "404.html": {
             "404_page": True,
             "title": "Page not found &#8212; PennyLane",
@@ -169,7 +224,7 @@ def render_templates():
             "canonical_url": "https://pennylane.ai/carnival/faq.html",
             "description": "Curious? Find all the details about PennyLane Quantum Carnival.",
             "thumbnail": "https://pennylane.ai/img/carnival_logo_splash.png",
-            "faq": FAQ,
+            "faq": CARNIVAL_FAQ,
             "root": ".."
         },
         "carnival/register.html": {
